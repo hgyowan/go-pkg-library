8409c0c181452d5ac283d05a99c1929401c64946
hgyowan <ghkdrydhsk@gmail.com>
2025-11-30 19:30:24 +0900

    feat: log 에 trace_id, span_id 추가

--- grpc-library/grpc/error_interceptor.go
+++ grpc-library/grpc/error_interceptor.go
@@ -9,6 +9,7 @@ import (
 	pkgLogger "github.com/hgyowan/go-pkg-library/logger"
 	otelCodes "go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -22,13 +23,19 @@ func CustomErrorUnaryInterceptor(
 ) (resp interface{}, err error) {
 	resp, err = handler(ctx, req)
 	if err != nil {
+		var traceID string
+		var spanID string
 		span := trace.SpanFromContext(ctx)
 		if span != nil {
 			span.RecordError(err)
 			span.SetStatus(otelCodes.Code(codes.Internal), err.Error())
+
+			sc := span.SpanContext()
+			traceID = sc.TraceID().String()
+			spanID = sc.SpanID().String()
 		}
 
-		pkgLogger.ZapLogger.Logger.Error(err.Error())
+		pkgLogger.ZapLogger.Logger.Error(err.Error(), zap.String("trace_id", traceID), zap.String("span_id", spanID))
 		castedErr, ok := pkgError.CastBusinessError(err)
 		if ok {
 			b, _ := json.Marshal(castedErr.Status)
@@ -50,13 +57,19 @@ func CustomErrorStreamInterceptor(
 ) (err error) {
 	err = handler(srv, req)
 	if err != nil {
+		var traceID string
+		var spanID string
 		span := trace.SpanFromContext(req.Context())
 		if span != nil {
 			span.RecordError(err)
 			span.SetStatus(otelCodes.Code(codes.Internal), err.Error())
+
+			sc := span.SpanContext()
+			traceID = sc.TraceID().String()
+			spanID = sc.SpanID().String()
 		}
 
-		pkgLogger.ZapLogger.Logger.Error(err.Error())
+		pkgLogger.ZapLogger.Logger.Error(err.Error(), zap.String("trace_id", traceID), zap.String("span_id", spanID))
 		castedErr, ok := pkgError.CastBusinessError(err)
 		if ok {
 			b, _ := json.Marshal(castedErr.Status)
